c3ed0bf58cfed0eb11233419c0343bf455aef7dd
Tra Pham <trapham.dev@gmail.com>
2026-08-07 09:23:12 +0700

    fix bug, ui

--- backend/ITHunterview.Service/UseCase/WalletUseCase.cs
+++ backend/ITHunterview.Service/UseCase/WalletUseCase.cs
@@ -341,8 +341,10 @@ namespace ITHunterview.Service.UseCase
                 }
 
                 amount = sub.Price;
+                // Parse features to get CoinCredit
+                var features = System.Text.Json.JsonSerializer.Deserialize<DTOs.Subscription.FeaturesConfigDto>(sub.FeaturesConfig);
                 // Snapshot coin bonus at purchase time so future package edits do not affect this payment.
-                creditsGranted = features.CoinCredit ?? 0;
+                creditsGranted = features?.CoinCredit ?? 0;
                 // Ánh xạ int ID thành Guid: 00000000-0000-0000-0000-XXXXXXXXXXXX
                 targetIdGuid = Guid.Parse(sub.Id.ToString().PadLeft(32, '0'));
                 descriptionText = $"Mua goi {sub.Name}";
