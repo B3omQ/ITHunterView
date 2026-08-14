f00d302e59a52c41d417b389843db6bf03d6ac82
Lilystoner <hainam25072004@gmail.com>
2026-08-14 09:32:32 +0700

    remove seed 2

--- backend/ITHunterview.Service/Infrastructure/Persistence/Migrations/20260813102322_RemoveJobSeeds.cs
+++ backend/ITHunterview.Service/Infrastructure/Persistence/Migrations/20260813102322_RemoveJobSeeds.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,7 +10,18 @@ namespace ITHunterview.Service.Infrastructure.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+                DELETE FROM ""job_skill_requirements""
+                WHERE ""job_id"" IN (
+                    SELECT ""id"" FROM ""job_postings""
+                    WHERE (""title"" LIKE '%(RealisticSeedV2)%' OR ""title"" LIKE '%(RealisticSeed)%')
+                       OR (""income_text"" = 'Mức lương cạnh tranh, thỏa thuận theo năng lực' AND ""work_location_text"" = 'Làm việc tại văn phòng công ty')
+                );
 
+                DELETE FROM ""job_postings"" 
+                WHERE (""title"" LIKE '%(RealisticSeedV2)%' OR ""title"" LIKE '%(RealisticSeed)%')
+                   OR (""income_text"" = 'Mức lương cạnh tranh, thỏa thuận theo năng lực' AND ""work_location_text"" = 'Làm việc tại văn phòng công ty');
+            ");
         }
 
         /// <inheritdoc />
